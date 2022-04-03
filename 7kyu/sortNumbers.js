fd03c30a6a7b779c336a7bc4bf080cbdabb72c27
jrgiacone <giaconejoshua@gmail.com>
2022-04-03 10:31:45 -0700

    Adds instructions and comments to the codewars challange Sort Numbers

--- 7kyu/sortNumbers.js
+++ 7kyu/sortNumbers.js
@@ -1,3 +1,15 @@
+/*Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+For example:
+
+solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+solution(null); // should return []
+
+*/
+
+
 function solution(nums){
   return nums === null ? [] : sort=nums.sort((a,b)=>a-b);
 } 
+
+/* The function above takes in an array of numbers in a random order and sorts all the numbers from smallest to biggest. If the array entered is null, then the function will return and empy array. */
