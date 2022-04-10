7d9e0b363525147fc446c6d115963c1b79c9ad83
jrgiacone <giaconejoshua@gmail.com>
2022-04-10 08:45:20 -0700

    4/10/22 Codewars to capitilize either the odd or even terms in a string

---
+++ 7kyu/alternateCapitalization.js
@@ -0,0 +1,34 @@
+/*
+Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+The input will be a lowercase string with no spaces.
+
+Good luck!
+
+If you like this Kata, please try:
+
+Indexed capitalization
+
+Even-odd disparity
+*/
+
+//My Solution:
+//
+function capitalize(s){
+  let odd = '';
+  let even = ''
+  
+  for ([i] of [...s].entries()){
+    if (i%2===0){
+      even += s[i].toUpperCase();
+      odd += s[i];
+    }else{
+      even += s[i];
+      odd += s[i].toUpperCase();
+    }
+  }
+  
+  return [even, odd]
+};
