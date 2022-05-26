035b99374dd38f7f70d70bd7b2c68743b0dd212b
jrgiacone <giaconejoshua@gmail.com>
2022-05-25 21:40:52 -0700

    5/25/2022 6kyu finds two numbers in the array that sum to the target number and returns their indexes

---
+++ 6kyu/twoSums.js
@@ -0,0 +1,9 @@
+function twoSum(numbers, target) {
+  for (let i = 0; i <=numbers.length; i++){
+    for (let j = 1; j <=numbers.length-1;j++){
+      if(numbers[i] + numbers[j] === target){
+        return [i,j]
+      }
+    }
+  }
+}
