9c698625cba1aa28640a45c438b7a7822fde2381
jrgiacone <giaconejoshua@gmail.com>
2022-08-02 21:10:10 -0700

    Leetcode twoSums practice using the Map function

---
+++ Leetcode/twoSum.js
@@ -0,0 +1,12 @@
+const twoSum = (nums, target) => {
+  let map = new Map();
+  
+  for (i = 0; i < nums.length; i++){
+      if(map.has(target-nums[i])){
+          return [map.get(target - nums[i]), i]
+      }else{
+          map.set(nums[i], i)
+      }
+  }
+  return [];
+}
