7f06d075949cf40d31a26b6bd1ca826bb657d412
jrgiacone <giaconejoshua@gmail.com>
2022-10-25 20:02:35 -0700

    Codewars 8kyu combine two sorted arrays

---
+++ 8kyu/mergeArray.js
@@ -0,0 +1,10 @@
+const mergeArrays = (arr1, arr2) => {
+  let arr = arr1.concat(arr2).splice((a, b) => a - b);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      arr.splice(i, 1)
+    }
+  }
+  return arr;
+}
+
